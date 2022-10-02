e505dbb3d1cba3bed5366de17f8e86c69bfc52fb
JJB64 <111893093+JJB64@users.noreply.github.com>
2022-10-02 21:15:15 +0000

    Update Node.java

--- Node.java
+++ Node.java
@@ -53,21 +53,21 @@ public class Node {
             int Tpath_cost=0;
             System.out.println(var);
             while(var.parent !=  null) {
-                flightpath= var.actions.getAirline() + " from " + var.getParent().getState().getIATA() + " to "+ var.state.getIATA() + "is " + var.actions.getStops() + "stops" ;
+                flightpath= var.actions.getAirline() + " from " + var.getParent().getState().getIATA() + " to "+ var.state.getIATA() + "is " + var.actions.getStops() + " stops" +"\r\n" ;
                 Cummulative_States.add(flightpath);
                 var = var.parent;
                 Tpath_cost +=1;
             }
-            String TotalFlights= "Total flights to take" + Tpath_cost;
+            String TotalFlights= "Total flights to take: " + Tpath_cost;
             Collections.reverse(Cummulative_States);
 
             try {
                 FileWriter output = new FileWriter("FlightPath.txt");
                 for(int i = 0; i < Cummulative_States.size(); i++){
-                    String data=i+". "+ Cummulative_States.get(i);
+                    String data= i+1 +". "+ Cummulative_States.get(i);
                     output.write(data);
                 }
-                output.write(Tpath_cost);
+                output.write(TotalFlights);
                 output.close();
                 System.out.println("Successfully wrote to the file.");
              }catch (IOException IOE) {
